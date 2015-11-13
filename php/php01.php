387faf14d340a1d212084c4e6c7a383edf00fb30
Francisco Quesada <pacoquesmun@gmail.com>
2015-11-13 17:19:43 +0100

    Añado los php

--- php/php01.php
+++ php/php01.php
@@ -1,26 +1,30 @@
-require_once ‘KalturaClient.php’;
- 
-$kalturaConfig = new KalturaConfiguration(123);
+<?
+function foo()
+{
+    require_once ‘KalturaClient.php’;
+
+$kalturaConfig = new KalturaConfiguration(2035151);
 // where 123 is your partner ID
- 
+
 $kalturaConfig->serviceUrl = ‘http://KalturaServerDomain’;
-// if you want to communicate with a Kaltura server which is 
+// if you want to communicate with a Kaltura server which is
 //    other than the default http://www.kaltura.com
- 
+
 $kalturaClient = new KalturaClient($kalturaConfig);
- 
-$ks = $kalturaClient->generateSession($adminSecret, $userId, KalturaSessionType::ADMIN, $partnerId, $expiry,$privileges);
+
+$ks = $kalturaClient->generateSession($adminSecret, $userId, KalturaSessionType::ADMIN, $partnerId, $expiry, $privileges);
 $kalturaClient->setKs($ks);
- 
-$entryId = ‘XXXYYYZZZA’;
+
+$entryId = ‘1_4ub4dahe’;
 // a known ID of media entry that you have
- 
-try
-{
-$mediaEntry = $client->media->get($entryId);
-echo $mediaEntry->name;
+
+try {
+    $mediaEntry = $client->media->get($entryId);
+    echo $mediaEntry->name;
+} catch (Exception $ex) {
+    echo “could not get entry from Kaltura . Reason: “ . $ex->getMessage();
 }
-catch(Exception $ex)
-{
-    echo “could not get entry from Kaltura. Reason: “ . $ex->getMessage();
 }
+
+?>
+
